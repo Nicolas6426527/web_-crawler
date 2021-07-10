82fbf8a014efd341915fb520b8af52c0c216dddc
Nicolas6426527 <n6426527@gmail.com>
2021-07-10 16:07:17 +0800

    day 26 多個網站爬取

--- HW/Day_026/ettoday.py
+++ HW/Day_026/ettoday.py
@@ -1,14 +1,16 @@
+# -*- coding: utf-8 -*-
 import scrapy
 from scrapy_demo.items import ScrapyDemoItem
+
 
 class EttodaySpider(scrapy.Spider):
     name = 'ettoday'
     allowed_domains = ['www.ettoday.net']
     start_urls = ['https://www.ettoday.net/news/20201004/1824032.htm',
-                 'https://www.ettoday.net/news/20201009/1826868.htm']
+                  'https://www.ettoday.net/news/20201009/1826868.htm']
 
     def parse(self, response):
         item = ScrapyDemoItem()
-        item['title']=response.xpath("//h1[@class='title']/text()").get() 
-        item['content'] = response.xpath("//div[@class='story']/p/text()").getall()
-        yield item 
+        item['title'] = response.xpath('//title/text()').get()
+        item['content'] = response.xpath('//div[@itemprop="articleBody"]//p/text()').getall()
+        yield item
--- HW/Day_026/settings.py
+++ HW/Day_026/settings.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
+
 # Scrapy settings for scrapy_demo project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
 #
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = 'scrapy_demo'
 
@@ -19,15 +21,17 @@ NEWSPIDER_MODULE = 'scrapy_demo.spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
-FEED_FORMAT="json" #可使用的格式 xml \ json \csv\ jsonl
-FEED_EXPORT_ENCODING ="utf-8" 
-FEED_URI = './output/ettoday.json' #.xml \ .json \ .csv \ .jsonl
+FEED_FORMAT = 'xml'
+FEED_EXPORT_ENCODING = 'utf-8'
+FEED_URI = './output/ettoday.xml'
+
 LOG_FILE = './output/ettoday.log'
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
@@ -47,31 +51,31 @@ LOG_FILE = './output/ettoday.log'
 #}
 
 # Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
 #    'scrapy_demo.middlewares.ScrapyDemoSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
 #    'scrapy_demo.middlewares.ScrapyDemoDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
 #EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
 
 # Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
 #    'scrapy_demo.pipelines.ScrapyDemoPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
@@ -84,7 +88,7 @@ LOG_FILE = './output/ettoday.log'
 #AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 #HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
